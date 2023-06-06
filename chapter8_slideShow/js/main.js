1227f8d219e18bee4b72f135c8a258c8a6a96383
lhjwork <jin.come.up.business@gmail.com>
2023-06-06 14:06:58 +0900

    8강 기초 슬라이드 구현까지 완료

--- chapter8_slideShow/js/main.js
+++ chapter8_slideShow/js/main.js
@@ -4,6 +4,21 @@ $(function () {
             slideCount = slides.length, 
             currentIndex = 0; 
 
+            // 해당시간이 지나면 한번만 할일
+            // setTimeout(할일, 시간);
+            // var timer = setTimeout(할일 , 1000);
+            // 시간을 한번 제서 멈출려고 하면
+            // clearTimeout(대상의 이름)
+             // clearTimeout(timer);
+
+            // 일정시간 마다 할일
+            // var timer = setInterval(할일, 시간);
+            // clearTimeout(timer);
+            // jquery 집합개체 (object)중 특정번째 요소를 선택 
+            // .eq(숫자) eq = equivalent의 약자  
+
+            // fadeIn() -> 요소를 서서히 나타나도록
+            // fadeOut() -> 요소를 서서히 사라지도록
             slides.eq(currentIndex).fadeIn();			
 			
 		var timer = undefined;  //타이머의 값을 undefined(지정되어 있지 않다)라고 지정합니다.
@@ -13,6 +28,8 @@ $(function () {
 		}         
 
             function showNextSlide(){
+              // 현재 슬라이드가 사라지고
+              // 다음 슬라이드가 나타난다.
 
               var nextIndex = (currentIndex + 1) % slideCount;
 
@@ -37,9 +54,11 @@ $(function () {
             }
 
             slides.mouseenter(function(){
+              // clearInterval(timer);
               timeroff();
             })
             .mouseleave(function(){
+              // setInterval(showNextSlide, 3500);
               timeron();
             })
 
